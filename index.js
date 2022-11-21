bc92472eabc931f5757e4573b86d3b55ec7d8d74
Megan <esler6724@gmail.com>
2022-11-20 17:58:38 -0800

    commit tree 6fbea7be2c585dedc33d4f1958c3c3f84e7e2b0e

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+// function superbowlWin(record, finder) {
+//     let result = null
+//     for(let team of record) {
+//         if(finder(team) === true){
+//             result = team
+//             break
+//         }
+//     }
+//     return result
+// }
+// const result = record.find(result => result === "W");
+
+function superbowlWin(record) {
+    const winner = record.find(item => item.result === "W");
+    console.log(winner);
+    if(winner) {
+        return winner.year 
+    }
+}
